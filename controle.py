e57d71ec179305eb5a2bde5877a090393f008a60
Tiago Souza <tiago.souza9935@gmail.com>
2021-01-10 19:42:40 -0300

    Ajustes

--- controle.py
+++ controle.py
@@ -9,6 +9,8 @@ banco = mysql.connector.connect(
     database="cadastro_produtos"
 )
 
+
+
 #Função para o botão 'Enviar', que guarda os dados recebidos do formulario em um banco de dados
 def funcao_principal():
     linha1 = formulario.lineEdit.text()
@@ -18,12 +20,9 @@ def funcao_principal():
 
     if formulario.radioButton.isChecked() :
         categoria = "Informatica"
-        formulario.radioButton.setCheckable(False)
     elif formulario.radioButton_2.isChecked() :
         categoria = "Alimentos"
-        formulario.radioButton_2.setCheckable(False)
     elif formulario.radioButton_3.isChecked() :
-        formulario.radioButton_3.setCheckable(False)
         categoria = "Eletronicos"
     else:
         categoria = "Nao Especificada"
@@ -40,14 +39,36 @@ def funcao_principal():
     print(f"Categoria {categoria} Selecionada")
 
     #Limpa o que esta escrito no formulario para um novo cadastro
-    formulario.lineEdit.clear()
-    formulario.lineEdit_2.clear()
-    formulario.lineEdit_3.clear()
+    formulario.lineEdit.setText("")
+    formulario.lineEdit_2.setText("")
+    formulario.lineEdit_3.setText("")
+
+
 
 #Função para o botão 'Listar', que abre uma nova janela com a listagem dos produtos
 def funcao_listar():
     listagem.show()
-        
+
+    cursor = banco.cursor()
+    comando_SQL = "select * from produtos"
+    cursor.execute(comando_SQL)
+    dados_lidos = cursor.fetchall()
+
+    listagem.tableWidget.setRowCount(len(dados_lidos))
+    listagem.tableWidget.setColumnCount(4)
+
+    listagem.tableWidget.setHorizontalHeaderItem(0, QtWidgets.QTableWidgetItem("Código"))
+    listagem.tableWidget.setHorizontalHeaderItem(1, QtWidgets.QTableWidgetItem("Descrição"))
+    listagem.tableWidget.setHorizontalHeaderItem(2, QtWidgets.QTableWidgetItem("Preço"))
+    listagem.tableWidget.setHorizontalHeaderItem(3, QtWidgets.QTableWidgetItem("Categoria"))
+
+    for i in range(0, len(dados_lidos)):
+        listagem.tableWidget.setVerticalHeaderItem(i, QtWidgets.QTableWidgetItem(str(dados_lidos[i][0])))
+        for j in range(1, 5):
+            listagem.tableWidget.setItem(i, j-1 ,QtWidgets.QTableWidgetItem(str(dados_lidos[i][j])))
+    
+
+
 #carrega os arquivos de interface do sistema
 app=QtWidgets.QApplication([])
 formulario=uic.loadUi("formulario.ui")
